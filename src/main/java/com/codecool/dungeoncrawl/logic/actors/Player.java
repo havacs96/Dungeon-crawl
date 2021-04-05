bd4e208cfe5a7c871de8898c01e88d1ed90ee217
Havacs Peter <havacs.peter@gmail.com>
2021-04-05 18:57:23 +0200

    reset

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -3,16 +3,12 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Player extends Actor {
-<<<<<<< HEAD
     private String name;
 
-=======
->>>>>>> dungeon-crawl-1-java-technorbi/master
     public Player(Cell cell) {
         super(cell);
     }
 
-<<<<<<< HEAD
     public Player(Cell cell, String name) {
         super(cell);
         this.name = name;
@@ -26,8 +22,6 @@ public class Player extends Actor {
         this.name = name;
     }
 
-=======
->>>>>>> dungeon-crawl-1-java-technorbi/master
     public String getTileName() {
         return "player";
     }
