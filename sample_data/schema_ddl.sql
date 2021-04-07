f5d3a7612746e6aa3ab60811c4c991ce467be7d4
ronairudi <ronai.rudi98@gmail.com>
2021-04-07 11:53:16 +0200

    feat: extended table columns

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS public.game_state;
-DROP TABLE IF EXISTS public.player;
-DROP TABLE IF EXISTS public.player_inventory;
+DROP TABLE IF EXISTS public.game_state CASCADE;
+DROP TABLE IF EXISTS public.player CASCADE;
+DROP TABLE IF EXISTS public.player_inventory CASCADE;
 
 
 CREATE TABLE public.game_state (
-    id serial NOT NULL PRIMARY KEY,
+    id serial NOT NULL,
     current_map integer NOT NULL,
     map1 text NOT NULL,
     map2 text NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE public.game_state (
 );
 
 CREATE TABLE public.player (
-    id serial NOT NULL PRIMARY KEY,
+    id serial NOT NULL,
     player_name text NOT NULL,
     strength text NOT NULL,
     health integer NOT NULL,
@@ -23,11 +23,18 @@ CREATE TABLE public.player (
 );
 
 CREATE TABLE public.player_inventory (
-    id serial NOT NULL PRIMARY KEY,
+    id serial NOT NULL,
     player_id integer NOT NULL,
-
+    item_name text NOT NULL,
+    item_type text NOT NULL
 );
 
+ALTER TABLE ONLY public.game_state
+    ADD PRIMARY KEY (id);
+ALTER TABLE ONLY public.player
+    ADD PRIMARY KEY (id);
+ALTER TABLE ONLY public.player_inventory
+    ADD PRIMARY KEY (id);
 
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
