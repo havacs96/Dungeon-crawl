bde6f13d7867bb9dc3737354e4c882b90d6320c2
Havacs Peter <havacs.peter@gmail.com>
2021-04-08 14:38:00 +0200

    fix: deleted parentheses from add method's SQL command

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -39,7 +39,7 @@ public class GameStateDaoJdbc implements GameStateDao {
     @Override
     public void update(GameState state) {
         try (Connection conn = dataSource.getConnection()) {
-            String sql = "UPDATE game_state SET (current_map = ?, map1 = ?, map2 = ?, map3 = ?, saved_at = ?)" +
+            String sql = "UPDATE game_state SET current_map = ?, map1 = ?, map2 = ?, map3 = ?, saved_at = ?" +
                     "WHERE player_id LIKE ?";
             PreparedStatement st = conn.prepareStatement(sql);
             st.setInt(1, state.getCurrentMap());
