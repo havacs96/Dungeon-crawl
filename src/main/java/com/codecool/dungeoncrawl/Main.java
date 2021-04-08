6c62a27af63a450cfa2e703c54fd898b7e816c23
Havacs Peter <havacs.peter@gmail.com>
2021-04-08 14:38:43 +0200

    feat: added new lines between map strings

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -49,7 +49,7 @@ public class Main extends Application {
     MenuBar menuBar = new MenuBar();
     GameDatabaseManager dbManager;
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 
--- src/main/java/com/codecool/dungeoncrawl/logic/MapSaver.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapSaver.java
@@ -78,6 +78,7 @@ public class MapSaver {
                         throw new RuntimeException("Unrecognized character: ");
                 }
             }
+            mapText += "\n";
         }
         return mapText;
     }
